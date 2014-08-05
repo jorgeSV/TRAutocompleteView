41214daaf792b2cfd39e1516d4784bd66f4fced0
Jorge SV <amk114@gmail.com>
2014-08-05 13:44:01 +0200

    Update TRViewController.m

--- TRAutocompleteViewDemo/TRAutocompleteViewDemo/TRViewController.m
+++ TRAutocompleteViewDemo/TRAutocompleteViewDemo/TRViewController.m
@@ -45,7 +45,7 @@
     self.view.backgroundColor = [UIColor colorWithPatternImage:[[UIImage imageNamed:@"debut_dark"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)]];
 
     [_textField setLeftPadding:9];
-    _autocompleteView = [TRAutocompleteView autocompleteViewBindedTo:_textField usingSource:[[TRGoogleMapsAutocompleteItemsSource alloc] initWithMinimumCharactersToTrigger:3  language:@"es" apiKey:@"AIzaSyBfEbzhgPJh7rwk1PYzhcw9I-69EmcZ3CI" types:@"(cities)"] cellFactory:[[TRGoogleMapsAutocompletionCellFactory alloc] initWithCellForegroundColor:[UIColor lightGrayColor] fontSize:14] presentingIn:self];
+    _autocompleteView = [TRAutocompleteView autocompleteViewBindedTo:_textField usingSource:[[TRGoogleMapsAutocompleteItemsSource alloc] initWithMinimumCharactersToTrigger:3  language:@"es" apiKey:@"YOUR_API_KEY" types:@"(cities)"] cellFactory:[[TRGoogleMapsAutocompletionCellFactory alloc] initWithCellForegroundColor:[UIColor lightGrayColor] fontSize:14] presentingIn:self];
     
     _autocompleteView.topMargin = 0;
     //_autocompleteView.backgroundColor = [UIColor colorWithRed:(27) / 255.0f green:(27) / 255.0f blue:(27) / 255.0f alpha:1];
